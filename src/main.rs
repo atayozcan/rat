796789ffb52bc47dee368e8fd82015d50c4fdedf
Atay Özcan <atayozcan@pm.me>
2021-05-09 18:33:20 +0300

    Initialize the repo

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+struct Cli {
+	#[structopt(parse(from_os_str))]
+	path: PathBuf,
+}
+
+fn main() {
+	let args = Cli::from_args();
+
+	let read= match std::fs::read_to_string(&args.path){
+		Ok(contents)=> contents,
+		Err(err)=>{err.to_string()},
+	};
+	println!("{}",read);
+}
