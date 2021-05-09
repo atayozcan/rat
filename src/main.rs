b8f6f4b9b425a2a31d1771ce1558d7d4e78233e1
Atay Özcan <atayozcan@pm.me>
2021-05-09 18:46:32 +0300

    added test flag

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ use std::path::PathBuf;
 struct Cli {
 	#[structopt(parse(from_os_str))]
 	path: PathBuf,
+
+	#[structopt(short = "c", long)]
+	length: bool,
 }
 
 fn main() {
+	let path = std::env::args().nth(1).expect("no path given");
+	let flags = std::env::args().nth(2);
+
 	let args = Cli::from_args();
 
 	let read= match std::fs::read_to_string(&args.path){
 		Ok(contents)=> contents,
 		Err(err)=>{err.to_string()},
 	};
-	println!("{}",read);
+
+	if args.length{
+		println!("{}",read.len())
+	} else { println!("{}",read) }
 }
