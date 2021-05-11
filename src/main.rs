adb2a686e46ac4b8634942924983515f97572800
Atay Özcan <atayozcan@pm.me>
2021-05-11 20:40:58 +0300

    added some new arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::borrow::{Borrow, BorrowMut};
 use std::fs;
 use std::path::PathBuf;
 use structopt::clap::AppSettings;
@@ -16,6 +15,15 @@ struct Cli {
 
     #[structopt(short = "E", long)]
     show_ends: bool,
+
+    #[structopt(short = "T", long)]
+    show_tabs: bool,
+
+    #[structopt(short = "s", long)]
+    squeeze_blank: bool,
+
+    #[structopt(short = "b", long)]
+    number_nonblank: bool,
 }
 
 fn main() {
@@ -41,6 +49,12 @@ fn main() {
         println!("{}", number(&mut read))
     } else if args.show_ends {
         println!("{}", show_ends(&mut read));
+    } else if args.show_tabs {
+        println!("{}", show_tabs(&mut read))
+    } else if args.squeeze_blank {
+        println!("{}", squeeze_blank(&mut read))
+    } else if args.number_nonblank {
+        println!("{}", number_nonblank(&mut read))
     } else {
         println!("{}", read)
     }
@@ -48,7 +62,7 @@ fn main() {
 
 fn generate_completions() {
     match fs::read(std::env::current_exe().unwrap()) {
-        Ok(..) => return,
+        Ok(..) => {}
         Err(..) => {
             Cli::clap().gen_completions(
                 env!("CARGO_PKG_NAME"),
@@ -69,15 +83,6 @@ fn generate_completions() {
     }
 }
 
-fn spaces(digits: usize) -> String {
-    let times = 6 - digits;
-    let mut space = "".to_string();
-    for _ in 0..times {
-        space.push(' ');
-    }
-    space
-}
-
 fn number(read: &mut String) -> String {
     let mut i = 1;
     let mut new_read = String::new();
@@ -91,7 +96,7 @@ fn number(read: &mut String) -> String {
         new_read.push('\n');
         i += 1;
     }
-    new_read
+    new_read.trim_end().to_string()
 }
 
 fn show_ends(read: &mut String) -> String {
@@ -100,6 +105,43 @@ fn show_ends(read: &mut String) -> String {
         new_read.push_str(line);
         new_read.push('$');
         new_read.push('\n');
+    }
+    new_read
+}
+
+fn show_tabs(read: &mut String) -> String {
+    read.replace("\t", "^I")
+}
+
+fn squeeze_blank(read: &mut String) -> String {
+    read.trim_matches('\n').to_string()
+}
+
+fn spaces(digits: usize) -> String {
+    let times = 6 - digits;
+    let mut space = "".to_string();
+    for _ in 0..times {
+        space.push(' ');
+    }
+    space
+}
+
+fn number_nonblank(read: &mut String) -> String {
+    let mut i = 1;
+    let mut new_read = String::new();
+    let digits = &read.lines().count().to_string().len();
+    for line in read.lines() {
+        if !line.is_empty() {
+            new_read.push_str(spaces(*digits).as_str());
+            let s = format!("{:>d$}", &*i.to_string(), d = digits);
+            new_read.push_str(&*s);
+            new_read.push('\t');
+            new_read.push_str(line);
+            new_read.push('\n');
+            i += 1;
+        } else {
+            new_read.push('\n');
+        }
     }
     new_read
 }
